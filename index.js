a452fa58d80c05728f253886f522c3647cdda9b9
dvkr <v3dvkr@gmail.com>
2023-08-15 13:27:07 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+
+const urlTable = [
+    {
+        "resourceName": "asdf",
+        "feedback": {
+            "2": {
+                "likes": 0,
+                "dislikes": 2
+            },
+            "13": {
+                "likes": 1,
+                "dislikes": 0
+            },
+            "17":  {
+                "likes": 1,
+                "dislikes": 0
+            },
+            "17.1":  {
+                "altName": "super review front end",
+                "likes": 1,
+                "dislikes": 0
+            },
+            "21": {
+                "likes": 1,
+                "dislikes": 0
+            }
+        }
+    }
+]
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get("/urltable", (req, res) => {
+	console.log({ urlTable })
+	console.log(urlTable[0]?.feedback)
+	res.send(urlTable)
+})
+
+app.get("/n/:num", (req, res) => {
+    const num = req.params.num
+    
+    res.send(urlTable[0].feedback[num])
+    // urlTable.filter()
+})
+
+app.get("/:str", (req, res) => {
+	const str = req.params.str
+	res.send("recieved: " + str)
+})
+
+app.post("/:str", (req, res) => {
+	console.log("POST: ", req.params.str)
+	const { str } = req.params
+	console.log("BODY: ", req.body)
+	const { classNum, isLiked } = req.body
+	// if (!urlTable.hasOwnProperty(req.params.str)) {
+	// 	urlTable[req.params.str] = { classNum, isLiked };
+	// }
+	const resourceIdx = urlTable.findIndex((el) => el.resourceName === str)
+	if (resourceIdx === -1) {
+		urlTable.push({
+			resourceName: str,
+			feedback: {
+				[classNum]: { likes: Number(!!isLiked), dislikes: Number(!isLiked) },
+			},
+		})
+	} else {
+		const votes = urlTable[resourceIdx].feedback[classNum]
+        if (votes) {
+            const {likes, dislikes} = votes
+            if (likes) {
+                urlTable[resourceIdx].feedback[classNum].likes += Number(!!isLiked)
+            } else if (dislikes) {
+                urlTable[resourceIdx].feedback[classNum].dislikes += Number(!isLiked)
+            }
+        }
+         else {
+            urlTable[resourceIdx].feedback[classNum] = { likes: Number(!!isLiked), dislikes: Number(!isLiked) }
+        }
+	}
+	res.send("posted")
+})
+
+app.get("/", (req, res) => {
+	res.send("Hello World!")
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
